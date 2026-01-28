550d87548930cd3bd483c810b0960976e7f95376
punnpunn11 <kevinray1323@yahoo.com>
2026-01-27 19:13:02 -0600

    Remove sparkle text click event listener

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.querySelector(".sparkle-text").addEventListener("click", function (event) {
-    for (let i = 0; i < 10; i++) {
-        let sparkle = document.createElement("div");
-        sparkle.classList.add("sparkle");
-        document.body.appendChild(sparkle);
 
-        let x = event.clientX + (Math.random() * 30 - 15);
-        let y = event.clientY + (Math.random() * 30 - 15);
-
-        sparkle.style.left = `${x}px`;
-        sparkle.style.top = `${y}px`;
-
-        setTimeout(() => sparkle.remove(), 500);
-    }
-});
